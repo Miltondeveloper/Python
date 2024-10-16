f755ba23ee241db27d5a6c2bab04e687746f3754
Miltondeveloper <84580850+Miltondeveloper@users.noreply.github.com>
2024-10-16 19:54:51 -0300

    Update atv2.py
    Adicionado botão

--- atv2.py
+++ atv2.py
@@ -1,5 +1,9 @@
 import tkinter as tk
-import tkinter.messagebox as tkm
+from tkinter.messagebox import showinfo
+
+def mostrar_mensagem():
+     showinfo("Informaçao", "Botão clicado!")
+    
 
 
 janela = tk.Tk()
@@ -8,9 +12,10 @@ janela.title("Interface gráfica com tkinter")
 
 janela.geometry("500x300")
 
-rotulo = tk.Label(janela, text="Bem vindo a sua primeira interface!")
-rotulo.pack()
+rotulo = tk.Label(janela, text="Bem vindo a sua primeira interface gráfica!")
+rotulo.pack(pady=20)
 
-button = button(janela, text="Botão clicado!")
+button = tk.Button(janela, text="Clique aqui", command = mostrar_mensagem)
+button.pack(pady=20)
 
 janela.mainloop()
